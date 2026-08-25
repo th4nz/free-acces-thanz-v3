f8081e47f7d6680b4e8b5a376ca84fc80c10df2d
th4nz <0304archivee@gmail.com>
2026-08-25 11:14:32 +0700

    Delete js directory

--- js/supabase-client.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = process.env.SUPABASE_URL || window.SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY || window.SUPABASE_ANON_KEY;
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-// Simpan ke window agar bisa diakses dari HTML
-window.supabase = supabase;
